d5e9ead1b284200dceddf01991567c3afd572592
Raydone <b.milliet@me.com>
2019-07-22 21:40:43 +0200

    create htaccess file

--- action.php
+++ action.php
@@ -8,6 +8,7 @@ fclose($fp);
  <html lang="fr">
  	<head>
  		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
  		<link rel="stylesheet" href="./css/bootstrap.min.css"/>
     	<link rel="stylesheet" href="./css/style.css"/>
  		<title>SNCF wifi gratuit</title>
